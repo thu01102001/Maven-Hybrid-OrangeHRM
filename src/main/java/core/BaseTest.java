234e66f43df2da1f937c7590543209847f57fb62
unknown <nguyenthilethu2001vn@gmail.com>
2025-06-19 18:02:41 +0700

    Multiple Browser

--- src/main/java/core/BaseTest.java
+++ src/main/java/core/BaseTest.java
@@ -1,4 +1,33 @@
 package core;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
 public class BaseTest {
+    private WebDriver driver;
+    protected WebDriver getBrowserNameDriver(String appUrl, String browserName) {
+        switch (browserName) {
+            case "Firefox":
+                driver = new FirefoxDriver();
+                break;
+            case "Chrome":
+                driver = new ChromeDriver();
+                break;
+            case "Edge":
+                driver = new EdgeDriver();
+                break;
+            default:
+                throw new RuntimeException("Browser name is not valid");
+        }
+        driver.get(appUrl);
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
+        return driver;
+    }
 }
